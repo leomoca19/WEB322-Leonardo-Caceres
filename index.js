fef35f718ab58f97b4f2720385981943bc614052
leomoca19 <leomoca19@gmail.com>
2023-09-13 03:11:07 -0400

    display red and yellow not working together

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ myEventEmitter.on('red', ()=>{
 })
 
 // Display "Yellow" for 2 seconds.
+myEventEmitter.on('yellow', ()=>{
+    console.log('yellow')
+    setTimeout(()=>{
+        console.log('')
+    }, 2000)
+})
+
 // Display "Green" for 5 seconds.
 // The cycle should repeat indefinitely.
 // Implement an event emitter to emit an event ```console.log('The light just changed')```` whenever the color changes.
 // Bonus: The event should include the current color as a parameter.
 
 myEventEmitter.emit('red')
+myEventEmitter.emit('yellow')
