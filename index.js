65d55d4a777deeca8b71e6058d36d82eeef39c39
Leonardo_Caceres <leomoca19@gmail.com>
2023-09-15 12:57:04 -0400

    event emitter implemented, error found in timeout

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ const lights = [
 
 let i = 0
 function lightChange() {
-    console.log(lights[i].color)
+    myEventEmitter.emit('change-lights')
     i = i === 2 ? 0 : i + 1 //i++ wouldn't work, i = i++ wouldn't work as expected
-    setTimeout(lightChange, lights[i].duration)
+    setTimeout(lightChange, lights[i].duration) //problem, each lights waits for the time of the next light because the index was incremented before this line is executed
 }
 
 // Implement an event emitter to emit an event ```console.log('The light just changed')```` whenever the color changes.
 // Bonus: The event should include the current color as a parameter.
-// myEventEmitter.on('')///////////
+myEventEmitter.on('change-lights', ()=>{
+    console.log(lights[i].color)
+    console.log('The light just changed')
+})
 
 lightChange()
