e6e0890d615d1b67f2aa794b690ac61cae29bfa8
Leonardo_Caceres <leomoca19@gmail.com>
2023-10-05 17:11:16 -0400

    1st route set

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 const PORT = 8080
 
-const users = [
+//Data structure
+const users = [ //should be changed to the database(jsons file) contents
     { id: 1, name: 'definetely a real person'},
     { id: 2, name: 'I am sooo a real person'},
     { id: 3, name: 'Realperson Jhonson'}
 ]
+
+
+
+
+//Routes
+app.get('/', (req, res)=>{
+    send('<h1>Log-in</h1>')
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
