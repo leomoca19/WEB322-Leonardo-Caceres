32a684fbf650c3e0c98e36d7f0ae0a46cdde975d
leomoca19 <leomoca19@gmail.com>
2023-10-17 18:40:34 -0400

    made pretier

--- routes/htmlUtils.js
+++ routes/htmlUtils.js
@@ -1,25 +1,26 @@
 //Bootstrap middleware
-const template = (title, html) => 
-    `<html>
-      <head>
-        <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-        crossorigin="anonymous"/>
-      </head>
-      <body class="container">
-        <nav>
-            <a href="/">Home</a>&nbsp;|&nbsp;<a href="/list">List</a>
-        </nav>    
-        <h1>${title}</h1>
-        <div>${html}</div> 
-      </body>
-      </html>`
+const template = (title, html) => `
+  <html>
+    <head>
+      <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+      crossorigin="anonymous"/>
+    </head>
+    <body class="container">
+      <nav>
+          <a href="/">Home</a>&nbsp;|&nbsp;<a href="/list">List</a>
+      </nav>    
+      <h1>${title}</h1>
+      <div>${html}</div> 
+    </body>
+  </html>
+  `
 
-      
-const htmlContent = (errorMsg = '') =>
-`<form method="POST">
+
+const htmlContent = (errorMsg = '') => `
+  <form method="POST">
     <div>${errorMsg}</div>
     <br>
 
@@ -32,6 +33,7 @@ const htmlContent = (errorMsg = '') =>
     <br>
 
     <button type="submit">GO!</button>
-</form>`
+  </form>
+  `
 
 module.exports = {template, htmlContent}
