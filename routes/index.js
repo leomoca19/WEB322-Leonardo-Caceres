49256a4b4ec0644b9e374d14d526519018ee77b6
leomoca19 <leomoca19@gmail.com>
2023-10-17 17:54:23 -0400

    users route updated and login added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const userRoutes = require('./users.routes')
+
+const loginRoutes = require('./login')
+const userRoutes = require('./users')
 
 const routes = express.Router()
+
 routes.use('/', userRoutes)
+routes.use('/users', loginRoutes)
 
 module.exports = routes
