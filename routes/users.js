b68b0a646a55acd5fca29ce757853c03b15844df
leomoca19 <leomoca19@gmail.com>
2023-10-17 17:53:29 -0400

    users.route.js renamed as users.js

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,10 @@ const express = require('express')
 const userRoutes = express.Router()
 const {template, htmlContent} = require('./htmlUtils')
 
+
 //Data structure
 const users = require('../data/fakeUsers')
 
-
-userRoutes.get('/', (req, res)=>{
-    res.send(template('Login', htmlContent()))
-})
-userRoutes.post('/', (req, res)=>{
-    const {username, password} = req.body
-    console.log(`user ${username} | pass ${password}`)
-
-    //change later for email and password from dbs
-    if (username === 'admin' && password === 'admin')
-        res.redirect('/list')
-    else
-        res.send(template('Login', htmlContent('Invalid credentials. Please try again')))
-})
 
 userRoutes.get('/list', (req,res)=>{
     const userList = users.map(users => 
