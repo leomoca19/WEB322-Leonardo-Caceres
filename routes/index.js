572161c2ed82591933dd633979705c95fe36dd45
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-13 16:35:59 -0500

    added route handler for api

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const loginRoutes = require('./login')
 const userRoutes = require('./users')
+const apiRoutes = require('./api')
 
 const routes = express.Router()
 
 routes.use('/', loginRoutes)
 routes.use('/users', userRoutes)
-
+routes.use('/api', apiRoutes)
 module.exports = routes
