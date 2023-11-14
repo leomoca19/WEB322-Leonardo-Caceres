86c203168b2e323469e7ba8dbc0eec9ed2696caa
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-13 19:03:44 -0500

    user routes in api

---
+++ routes/models/users.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const apiUsers = express.Router()
+const {template, userDetails} = require('../htmlUtils')
+
+const users = require('../../data/fakeUsers')
+
+apiUsers.get('/', (req, res) => 
+    res.send(template('Users', JSON.stringify(users)))
+)
+
+apiUsers.get('/:id', (req,res) =>{
+    const id = req.params.id
+    const user = users.find(user => user.id == id)
+
+    res.send(template('Details', userDetails(user)))
+})
+
+apiUsers.delete('/:id', (req,res) =>{
+    const id = req.params.id
+    const user = users.find(user => user.id == id)
+
+    users.filter((id) => id != user.id)
+})
+
+apiUsers.post('/', (req,res) =>{
+    let newUser = {...req.body} //verify if works as expected
+    users.push(newUser)
+    res.json.stringify(newUser)
+})
+
+module.exports = apiUsers
