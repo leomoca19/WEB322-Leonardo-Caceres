683b1c937312edfb8dd094fd8e50947de596af4e
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-23 15:49:53 -0500

    commented old users routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 
 const loginRoutes = require('./login')
-const userRoutes = require('./users')
+// const userRoutes = require('./users')
 const apiUsers = require('./models/users')
 const apiProducts = require('./models/products')
 
 const routes = express.Router()
 
 routes.use('/', loginRoutes)
-routes.use('/users', userRoutes)
+// routes.use('/users', userRoutes)
 routes.use('/api/users', apiUsers)
 routes.use('/api/products', apiProducts)
 
