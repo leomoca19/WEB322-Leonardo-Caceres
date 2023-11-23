c27ae2e9697938b5cd8039a1a228587c0c5e1518
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-23 15:35:05 -0500

    comments added for A4

--- server.js
+++ server.js
@@ -10,3 +10,35 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`))
+
+
+// const Sequelize = require('sequelize');
+
+// // set up sequelize to point to our postgres database
+// let sequelize = new Sequelize('neondb', 'leomoca19', '5O4lcByxPMep', {
+//   host: 'ep-sparkling-bird-59202610-pooler.us-east-2.aws.neon.tech',
+//   dialect: 'postgres',
+//   port: 5432,
+//   dialectOptions: {
+//     ssl: { rejectUnauthorized: false },
+//   },
+//   query: {raw: true}
+// })
+
+
+// // Define a "Project" model
+// const Project = sequelize.define('Project', {
+//     title: Sequelize.STRING,
+//     description: Sequelize.TEXT,
+// })
+
+
+// // create a new "Project"/row and add it to the database
+// sequelize.sync().then(() => 
+//     Project.create({
+//       title: 'Project1',
+//       description: 'First Project',
+//     })
+//     .then((project) => console.log('success!'))
+//     .catch((error) => console.log('something went wrong!'))
+// )
