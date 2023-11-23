62bb16d01aadd2f37965ca37a7e411f85e1e80b5
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-23 16:46:50 -0500

    included api router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
+const apiRoutes = require('./routes/api.routes')
 
 const app = express()
 const PORT = 8080
@@ -8,5 +9,7 @@ const PORT = 8080
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
+app.use('/api', apiRoutes)
+
 
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`))
