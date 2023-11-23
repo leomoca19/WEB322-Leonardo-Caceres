8b37674b511d0c94b7d13851104146673be72344
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-23 16:54:14 -0500

    moved api routes to api.routes.js

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,15 @@
 const express = require('express')
 
 const loginRoutes = require('./login')
-
 const userRoutes = require('./users')
 const productRoutes = require('./products')
-
-const apiUsers = require('./models/users')
-const apiProducts = require('./models/products')
 
 const routes = express.Router()
 
 
 routes.use('/', loginRoutes)
-
 routes.use('/users', userRoutes)
 routes.use('/products', productRoutes)
 
-routes.use('/api/users', apiUsers)
-routes.use('/api/products', apiProducts)
 
 module.exports = routes
