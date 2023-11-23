1230b34f2a04035a74513d32f531fec7c535df06
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-23 16:24:46 -0500

    added get, delete and post endpoints

--- routes/models/products.js
+++ routes/models/products.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const apiProducts = express.Router()
-const {template} = require('../htmlUtils')
 
 const products = require('../../data/fakeProducts')
 
 apiProducts.get('/', (req, res) => 
-    res.send(template('Products', JSON.stringify(products)))
+    res.send(products)
 )
+
+apiProducts.get('/:id', (req,res) =>{
+    const id = req.params.id
+    const product = products.find(product => product.id == id)
+
+    res.send(product)
+})
+
+apiProducts.delete('/:id', (req,res) =>{
+    const id = req.params.id
+    const product = products.find(product => product.id == id)
+
+    products.filter((id) => id != product.id)
+})
+
+apiProducts.post('/', (req,res) =>{
+    let newProduct = {...req.body} //verify if works as expected
+    products.push(newProduct)
+    res.send(newProduct)
+})
+
 
 module.exports = apiProducts
