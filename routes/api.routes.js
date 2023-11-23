07280a60c77e1fd364470b52fb66e2af540e8d54
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-23 17:19:54 -0500

    api login endpoint finished

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -11,4 +11,23 @@ apiRoutes.use('/users', apiUsers)
 apiRoutes.use('/products', apiProducts)
 
 
+apiProducts.post('/', (req, res) => {
+    let users = require('../../data/fakeUsers')
+    let {email, isAdmin} = {...req.body}
+
+
+    let status = 200
+    let result = {isAuthenticated: true}
+
+
+    if (!users.find(
+        user => user.email == email && user.isAdmin == isAdmin
+        )) {
+            result = {isAuthenticated: true}
+            status = 401
+        }
+
+   res.status(status).send(result)
+})
+
 module.exports = apiRoutes
