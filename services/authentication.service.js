9667aee72cfaca53539030d79e16cf5e06686fec
Leonardo_Caceres <leomoca19@gmail.com>
2023-11-30 18:41:48 -0500

    authentication function improved, not finished

--- services/authentication.service.js
+++ services/authentication.service.js
@@ -1,6 +1,11 @@
 class AuthenticationService {
-  static authenticate = (username, password) => 
-   ({ isAutheticated: true, token: "sasaxsdlcihsdoichisdjnc" })
-}
+  static authenticate = (username, password) => {
+    let authenticated = { isAutheticated: false}
+    
+    if(true /*user is admin or username macthes user password*/)
+      authenticated = { isAutheticated: true}
+
+   return authenticated
+}}
 
 module.exports = AuthenticationService;
