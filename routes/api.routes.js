25972d511f172c6983652c8289df1b6b8faeb6bb
leomoca19 <leomoca19@gmail.com>
2023-11-30 05:32:13 -0500

    A3 finnished

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,33 +1,14 @@
 const express = require('express')
 
+const apiLogin = require('.models/login')
 const apiUsers = require('./models/users')
 const apiProducts = require('./models/products')
 
+
 const apiRoutes = express.Router()
 
-
-// apiRoutes.use('/api/login', apiLogin)
+apiRoutes.use('/api/login', apiLogin)
 apiRoutes.use('/users', apiUsers)
 apiRoutes.use('/products', apiProducts)
-
-
-apiProducts.post('/', (req, res) => {
-    let users = require('../../data/fakeUsers')
-    let {email, isAdmin} = {...req.body}
-
-
-    let status = 200
-    let result = {isAuthenticated: true}
-
-
-    if (!users.find(
-        user => user.email == email && user.isAdmin == isAdmin
-        )) {
-            result = {isAuthenticated: true}
-            status = 401
-        }
-
-   res.status(status).send(result)
-})
 
 module.exports = apiRoutes
---
+++ routes/models/login.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const apiLogin = express.Router()
+
+const users = require('../../data/fakeUsers')
+
+apiProducts.post('/', (req, res) => {
+    let {email, isAdmin} = {...req.body}
+
+    let status = 200
+    let result = {isAuthenticated: true}
+
+    if (!users.find(user => user.email == email && user.isAdmin == isAdmin))
+    {
+        result = {isAuthenticated: true}
+        status = 401
+    }
+
+   res.status(status).send(result)
+})
+
+module.exports = apiLogin
