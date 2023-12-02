616b6f64a30d813e8cf4dd1f90028d4d4ab48558
leomoca19 <leomoca19@gmail.com>
2023-12-02 04:10:58 -0500

    switched back to using local data

--- routes/login.js
+++ routes/login.js
@@ -2,16 +2,19 @@ const express = require('express')
 const loginRoutes = express.Router()
 const {template, htmlContent} = require('./htmlUtils')
 
-const AuthenticationService = require('../services/authentication.service')
+const users = require('../data/fakeUsers.json')
 
-loginRoutes.get('/', (req, res)=>{
+loginRoutes.get('/', (req, res)=>
     res.send(template('Login', htmlContent()))
-})
+)
 
 loginRoutes.post('/', (req, res)=>{
-    const {username, password} = req.body
-        
-    if (AuthenticationService.authenticate(username, password))
+    let {username, password} = req.body
+    
+    let user = users.find((user) => user.username == username)
+
+    if (username === 'admin' && password === 'admin' 
+        || user && (user.password == password))
         res.redirect('/users')
     else
         res.send(template('Login', htmlContent('Invalid credentials. Please try again')))
