65432ada5d5df661b7f3d873565afc9a8dd7ee7e
leomoca19 <leomoca19@gmail.com>
2023-12-02 08:33:32 -0500

    made service calls async

--- routes/models/orders.js
+++ routes/models/orders.js
@@ -3,20 +3,20 @@ const apiOrders = express.Router()
 
 const OrdersService = require('../../services/orders.service')
 
-apiOrders.get('/', (req, res) => 
-    res.send(OrdersService.findAll())
+apiOrders.get('/', async (req, res) => 
+    res.send(await OrdersService.findAll())
 )
 
-apiOrders.get('/:id', (req,res) =>
-    res.send(OrdersService.findById(req.params.id))
+apiOrders.get('/:id', async (req,res) =>
+    res.send(await OrdersService.findById(req.params.id))
 )
 
-apiOrders.delete('/:id', (req,res) =>
-    res.send(OrdersService.delete(req.params.id))
+apiOrders.delete('/:id', async (req,res) =>
+    res.send(await OrdersService.delete(req.params.id))
 )
 
-apiOrders.post('/', (req,res) => 
-    res.send(OrdersService.create({...req.body}))
+apiOrders.post('/', async (req,res) => 
+    res.send(await OrdersService.create({...req.body}))
 )
 
 
