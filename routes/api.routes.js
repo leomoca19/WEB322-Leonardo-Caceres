be86ecf4e3ded272f12e146ae563cc7d97515b0d
leomoca19 <leomoca19@gmail.com>
2023-12-02 08:51:53 -0500

    added orders router

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,6 +3,7 @@ const express = require('express')
 const apiLogin = require('./models/login')
 const apiUsers = require('./models/users')
 const apiProducts = require('./models/products')
+const apiOrders = require('./models/orders')
 
 
 const apiRoutes = express.Router()
@@ -10,5 +11,6 @@ const apiRoutes = express.Router()
 apiRoutes.use('/api/login', apiLogin)
 apiRoutes.use('/users', apiUsers)
 apiRoutes.use('/products', apiProducts)
+apiOrders.use('/orders', apiOrders)
 
 module.exports = apiRoutes
