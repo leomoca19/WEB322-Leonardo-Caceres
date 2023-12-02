0ee77eab11d42e457de16c098df639a0214ab0de
leomoca19 <leomoca19@gmail.com>
2023-12-02 16:36:28 -0500

    trying to find error in orders

--- db.js
+++ db.js
@@ -54,90 +54,93 @@ orders.belongsTo(products)
 
 // Table population 
 const seedDatabase = async () => {
+    // Populate only if table is empty
     try {
         // Populate the Users table
-        await users.bulkCreate([
-        {
-            firstName: "Mossie",
-            lastName: "Mosciski",
-            email: "Nyah_OHara-Johns@gmail.com",
-            password: "upMyHwDHwjHJdIE",
-            dob: "2023-07-09T01:15:17.101Z",
-            company: "Murphy Inc",
-            phone: "967-963-0570 x9136",
-        },
-        {
-            firstName: "Karen",
-            lastName: "Yundt",
-            email: "Kristina68@hotmail.com",
-            password: "0p91XkC2iIy44CF",
-            dob: "2022-10-20T11:12:02.084Z",
-            company: "Kuhic Inc",
-            phone: "(796) 606-8670 x2136",
-        },
-        {
-            firstName: "Malachi",
-            lastName: "Dibbert",
-            email: "Brendon_Hahn72@yahoo.com",
-            password: "dvkXiXKZVbsJCtB",
-            dob: "2023-04-25T17:57:53.896Z",
-            company: "Sawayn - Johnston",
-            phone: "874-405-2158 x751",
-        },
-        {
-            firstName: "Elroy",
-            lastName: "Hansen",
-            email: "Kelsie.Beier@gmail.com",
-            password: "5Gn84pqXRYBFJo8",
-            dob: "2023-07-14T06:28:32.471Z",
-            company: "Bartell, Frami and Hand",
-            phone: "1-516-257-8571 x847",
-        },
-        {
-            firstName: "Lucious",
-            lastName: "Casper",
-            email: "Consuelo75@hotmail.com",
-            password: "A0_VSXKl1BFre2v",
-            dob: "2022-12-13T07:04:39.469Z",
-            company: "Lang - Kessler",
-            phone: "800-824-8685 x0095",
-        },
-        ])
+
+        if (await users.count() < 1) 
+            await users.bulkCreate([
+                {
+                    firstName: "Mossie",
+                    lastName: "Mosciski",
+                    email: "Nyah_OHara-Johns@gmail.com",
+                    password: "upMyHwDHwjHJdIE",
+                    dob: "2023-07-09T01:15:17.101Z",
+                    company: "Murphy Inc",
+                    phone: "967-963-0570 x9136",
+                },
+                {
+                    firstName: "Karen",
+                    lastName: "Yundt",
+                    email: "Kristina68@hotmail.com",
+                    password: "0p91XkC2iIy44CF",
+                    dob: "2022-10-20T11:12:02.084Z",
+                    company: "Kuhic Inc",
+                    phone: "(796) 606-8670 x2136",
+                },
+                {
+                    firstName: "Malachi",
+                    lastName: "Dibbert",
+                    email: "Brendon_Hahn72@yahoo.com",
+                    password: "dvkXiXKZVbsJCtB",
+                    dob: "2023-04-25T17:57:53.896Z",
+                    company: "Sawayn - Johnston",
+                    phone: "874-405-2158 x751",
+                },
+                {
+                    firstName: "Elroy",
+                    lastName: "Hansen",
+                    email: "Kelsie.Beier@gmail.com",
+                    password: "5Gn84pqXRYBFJo8",
+                    dob: "2023-07-14T06:28:32.471Z",
+                    company: "Bartell, Frami and Hand",
+                    phone: "1-516-257-8571 x847",
+                },
+                {
+                    firstName: "Lucious",
+                    lastName: "Casper",
+                    email: "Consuelo75@hotmail.com",
+                    password: "A0_VSXKl1BFre2v",
+                    dob: "2022-12-13T07:04:39.469Z",
+                    company: "Lang - Kessler",
+                    phone: "800-824-8685 x0095",
+                },
+                ])
 
         // Populate the Products table
-        await products.bulkCreate([
-        {
-            name: "Refined Rubber Pizza",
-            isbn: "978-0-653-89492-8",
-            price: "355.00",
-            description: "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
-        },
-        {
-            name: "Rustic Metal Mouse",
-            isbn: "978-1-302-07378-7",
-            price: "410.00",
-            description: "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
-        },
-        {
-            name: "Rustic Fresh Sausages",
-            isbn: "978-0-253-09153-6",
-            price: "601.00",
-            description: "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four-wheel drive",
-        },
-        {
-            name: "Handcrafted Steel Shirt",
-            isbn: "978-0-949478-82-5",
-            price: "757.00",
-            description: "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7-Color RGB LED Back-lighting for smart functionality",
-        },
-        {
-            name: "Ergonomic Frozen Cheese",
-            isbn: "978-1-61775-249-0",
-            price: "742.00",
-            description: "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
-        },
-        // Add more products as needed
-        ])
+        if (await products.count() < 1)
+            await products.bulkCreate([
+                {
+                    name: "Refined Rubber Pizza",
+                    isbn: "978-0-653-89492-8",
+                    price: "355.00",
+                    description: "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
+                },
+                {
+                    name: "Rustic Metal Mouse",
+                    isbn: "978-1-302-07378-7",
+                    price: "410.00",
+                    description: "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
+                },
+                {
+                    name: "Rustic Fresh Sausages",
+                    isbn: "978-0-253-09153-6",
+                    price: "601.00",
+                    description: "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four-wheel drive",
+                },
+                {
+                    name: "Handcrafted Steel Shirt",
+                    isbn: "978-0-949478-82-5",
+                    price: "757.00",
+                    description: "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7-Color RGB LED Back-lighting for smart functionality",
+                },
+                {
+                    name: "Ergonomic Frozen Cheese",
+                    isbn: "978-1-61775-249-0",
+                    price: "742.00",
+                    description: "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+                },
+                ])
       
         // Populate the Orders table
         const orderData = [
@@ -179,7 +182,15 @@ const seedDatabase = async () => {
         ]
   
         // Populate the Orders table
-        await orders.bulkCreate(orderData)
+        try {
+            // appearent bug here:
+            // orders is not populated
+            if (await orders.count() < 1) {
+                await orders.bulkCreate(orderData);
+                console.log('Orders table populated successfully');
+            } else {
+                console.log('Orders table already has data. Skipping population.');
+            }
 
         console.log('Database seeded successfully')
     }   
@@ -201,9 +212,7 @@ const connect = async () => {
 
 const sync = async () => {
     try {
-        // will remain commented after being populated
-        // await seedDatabase()
-
+        await seedDatabase()
         await sequelize.sync({alter: true})
         console.log('Models synchronized with the db.')
     } catch (err) {
--- services/users.service.js
+++ services/users.service.js
@@ -5,8 +5,24 @@ class UsersService{
 
     static findById = async (id) => users.findByPk(id)
 
-    static findByIdWithOrders = async (id) => users.findByPk(id, { include: orders })
+    static findByIdWithOrders = async (id) => {
+        try {
+            const userWithOrders = await users.findByPk(id, { include: orders })
     
+            if (userWithOrders) {
+                return userWithOrders
+            } else {
+                console.log(`User with id ${id} not found`)
+                return null  // or throw an error, depending on your requirements
+            }
+        } catch (error) {
+            console.error('Error in findByIdWithOrders:', error)
+            throw error  // Rethrow the error
+        }
+    }
+    
+    
+
     static findByEmail = async (email) => users.findOne({ where: { email }})
 
     // username is the full name of the user fname + ' ' + lname
