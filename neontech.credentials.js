e350083472d38a9660e770483c26cc5a80fc6f80
Leonardo_Caceres <leomoca19@gmail.com>
2023-12-07 15:55:40 -0500

    credentials to connect to db

---
+++ neontech.credentials.js
@@ -0,0 +1,11 @@
+PGHOST='ep-sparkling-bird-59202610-pooler.us-east-2.aws.neon.tech'
+PGDATABASE='neondb'
+PGUSER='leomoca19'
+PGPASSWORD='5O4lcByxPMep'
+
+module.exports = {
+    PGHOST,
+    PGDATABASE,
+    PGUSER,
+    PGPASSWORD
+}
