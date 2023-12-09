753adfd49bd116dff672d4380a95f6dd0f1deb00
leomoca19 <leomoca19@gmail.com>
2023-12-09 04:20:40 -0500

    service only checks if username is 'admin' and password is 'password'

--- services/authentication.service.js
+++ services/authentication.service.js
@@ -1,9 +1,6 @@
-const UsersService = require("./users.service")
-
 class AuthenticationService {
   static authenticate = async (username, password) => {
-    let user = await UsersService.findByUsername(username);
-    return user && ((user.isAdmin || user.password == password) || (username == 'admin' && password == 'password'))
+    this.isAuthenticated = username == 'admin' && password == 'password'
 }}
 
 module.exports = AuthenticationService;
