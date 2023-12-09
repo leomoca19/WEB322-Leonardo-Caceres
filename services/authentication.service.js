12d0c524b8f45345457d556fc9b52a4130dfa6ed
leomoca19 <leomoca19@gmail.com>
2023-12-09 05:07:03 -0500

    fixed return value of authenticate

--- services/authentication.service.js
+++ services/authentication.service.js
@@ -1,8 +1,10 @@
 class AuthenticationService {
   static isAuthenticated = false
 
-  static authenticate = async (username, password) => 
-    this.isAuthenticated = username === 'admin' && password === 'password';
+  static authenticate = (username, password) => { 
+    this.isAuthenticated = username === 'admin' && password === 'password'
+    return { isAuthenticated: this.isAuthenticated }
+  }
 }
 
 module.exports = AuthenticationService;
