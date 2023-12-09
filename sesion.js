038f0035dbf74ff0e3c17d6df2e92c5c3739d808
leomoca19 <leomoca19@gmail.com>
2023-12-09 04:24:03 -0500

    cookies for authentication

---
+++ sesion.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cookies = express()
+
+cookies.use(
+    clientSessions({
+        cookieName: "session",
+        secret: "SECRETCODE",
+        duration: 2 * 60 * 100000,
+        activeDuration: 1000 * 60,
+    })
+)
+
+
+const path = req.path
+if (path === "/" || path.includes("/api")) 
+  next()
+
+else {
+  if (req?.session?.auth?.isAuthenticated) 
+    next()
+  else 
+    res.redirect("/");
+}
+
+
+module.exports = cookies
